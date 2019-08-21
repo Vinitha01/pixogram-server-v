98425cd750c7e23a4c46eff8f3bf5bc60ed54c85
B <760864@cognizant.com>
2019-08-21 17:47:05 +0530

    last commit

---
+++ src/main/java/com/social/pixogram/service/UserService.java
@@ -0,0 +1,17 @@
+package com.social.pixogram.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import com.social.pixogram.model.User;
+
+
+public interface UserService {
+
+	public List<User> getUsers();
+	
+	public User createUser(User user);
+	
+	public Optional<User> getUserById(Long userId);
+}
